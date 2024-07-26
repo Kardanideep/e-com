eb25ad40ddbb068348a8e6929afc5e8dbb292b57
Deep Kardani <kardanideep1@gmail.com>
2024-07-26 17:54:45 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const path = require("path")
   
 // error middlware 
 const errorMiddleware = require("./middleware/error");
@@ -31,11 +30,6 @@ app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
-
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
-})
 
 // middleware for error
 app.use(errorMiddleware);
